8b447a9b961ed1a741ec041c39bb8934fbf00721
ericklealribeiro <erickleal.elr@gmail.com>
2023-09-19 06:46:07 -0300

    corrigi problema de width dos cards de section "xp", agora não tem mais incompatibilidade de responsividade para celulares

--- JavaScript/xpslide.js
+++ JavaScript/xpslide.js
@@ -7,49 +7,49 @@ function btnRight(){
     let nextDivId = 'xp'+ (count3 + 1);
     if (count3 == 5){
         count3 = 1;
-        document.getElementById('xp3').style.right='25%'
+        document.getElementById('xp3').style.right='45%'
         document.getElementById('xp3').style.opacity='0'
-        document.getElementById('xp1').style.right='75%'
+        document.getElementById('xp1').style.right='55%'
         document.getElementById('xp1').style.opacity='0'
         document.getElementById('xp2').style.right='50%'
         document.getElementById('xp2').style.opacity='1'
     } else if (nextDivId == 'xp1'){
         count3++;
-        document.getElementById('xp3').style.right='25%'
+        document.getElementById('xp3').style.right='45%'
         document.getElementById('xp3').style.opacity='0'
-        document.getElementById('xp1').style.right='75%'
+        document.getElementById('xp1').style.right='55%'
         document.getElementById('xp1').style.opacity='0'
         document.getElementById('xp2').style.right='50%'
         document.getElementById('xp2').style.opacity='1'
     } else if (nextDivId == 'xp2'){
         count3++;
-        document.getElementById('xp4').style.right='25%'
+        document.getElementById('xp4').style.right='45%'
         document.getElementById('xp4').style.opacity='0'
-        document.getElementById('xp2').style.right='75%'
+        document.getElementById('xp2').style.right='55%'
         document.getElementById('xp2').style.opacity='0'
         document.getElementById('xp3').style.right='50%'
         document.getElementById('xp3').style.opacity='1'
     } else if (nextDivId == 'xp3'){
         count3++;
-        document.getElementById('xp5').style.right='25%'
+        document.getElementById('xp5').style.right='45%'
         document.getElementById('xp5').style.opacity='0'
-        document.getElementById('xp3').style.right='75%'
+        document.getElementById('xp3').style.right='55%'
         document.getElementById('xp3').style.opacity='0'
         document.getElementById('xp4').style.right='50%'
         document.getElementById('xp4').style.opacity='1'
     } else if (nextDivId == 'xp4'){
         count3++;
-        document.getElementById('xp1').style.right='25%'
+        document.getElementById('xp1').style.right='45%'
         document.getElementById('xp1').style.opacity='0'
-        document.getElementById('xp4').style.right='75%'
+        document.getElementById('xp4').style.right='55%'
         document.getElementById('xp4').style.opacity='0'
         document.getElementById('xp5').style.right='50%'
         document.getElementById('xp5').style.opacity='1'
     } else if (nextDivId == 'xp5'){
         count3 = 0;
-        document.getElementById('xp2').style.right='25%'
+        document.getElementById('xp2').style.right='45%'
         document.getElementById('xp2').style.opacity='0'
-        document.getElementById('xp5').style.right='75%'
+        document.getElementById('xp5').style.right='55%'
         document.getElementById('xp5').style.opacity='0'
         document.getElementById('xp1').style.right='50%'
         document.getElementById('xp1').style.opacity='1'
@@ -58,12 +58,11 @@ function btnRight(){
 
 function btnLeft(){
     const nextDivLeft = 'xp'+ (count3 + 1);
-    console.log(nextDivLeft);
     if (nextDivLeft == 'xp1'){
         count3 = 4;
-        document.getElementById('xp1').style.right='25%'
+        document.getElementById('xp1').style.right='45%'
         document.getElementById('xp1').style.opacity='0'
-        document.getElementById('xp4').style.right='75%'
+        document.getElementById('xp4').style.right='55%'
         document.getElementById('xp4').style.opacity='0'
         document.getElementById('xp5').style.right='50%'
         document.getElementById('xp5').style.opacity='1'
@@ -71,46 +70,46 @@ function btnLeft(){
         count3 = 0;
         document.getElementById('xp1').style.right='50%'
         document.getElementById('xp1').style.opacity='1'
-        document.getElementById('xp5').style.right='75%'
+        document.getElementById('xp5').style.right='55%'
         document.getElementById('xp5').style.opacity='0'
-        document.getElementById('xp4').style.right='25%'
+        document.getElementById('xp4').style.right='45%'
     } else if (nextDivLeft == 'xp6'){
         count3--;
-        document.getElementById('xp1').style.right='25%'
+        document.getElementById('xp1').style.right='45%'
         document.getElementById('xp1').style.opacity='0'
-        document.getElementById('xp4').style.right='75%'
+        document.getElementById('xp4').style.right='55%'
         document.getElementById('xp4').style.opacity='0'
         document.getElementById('xp5').style.right='50%'
         document.getElementById('xp5').style.opacity='1'
     } else if (nextDivLeft == 'xp2'){
         count3 = 5;
-        document.getElementById('xp2').style.right='25%'
+        document.getElementById('xp2').style.right='45%'
         document.getElementById('xp2').style.opacity='0'
-        document.getElementById('xp5').style.right='75%'
+        document.getElementById('xp5').style.right='55%'
         document.getElementById('xp5').style.opacity='0'
         document.getElementById('xp1').style.right='50%'
         document.getElementById('xp1').style.opacity='1'
     } else if (nextDivLeft == 'xp3'){
         count3--;
-        document.getElementById('xp3').style.right='25%'
+        document.getElementById('xp3').style.right='45%'
         document.getElementById('xp3').style.opacity='0'
-        document.getElementById('xp1').style.right='75%'
+        document.getElementById('xp1').style.right='55%'
         document.getElementById('xp1').style.opacity='0'
         document.getElementById('xp2').style.right='50%'
         document.getElementById('xp2').style.opacity='1'
     } else if (nextDivLeft == 'xp4'){
         count3--;
-        document.getElementById('xp4').style.right='25%'
+        document.getElementById('xp4').style.right='45%'
         document.getElementById('xp4').style.opacity='0'
-        document.getElementById('xp2').style.right='75%'
+        document.getElementById('xp2').style.right='55%'
         document.getElementById('xp2').style.opacity='0'
         document.getElementById('xp3').style.right='50%'
         document.getElementById('xp3').style.opacity='1'
     } else if (nextDivLeft == 'xp5'){
         count3--;
-        document.getElementById('xp5').style.right='25%'
+        document.getElementById('xp5').style.right='45%'
         document.getElementById('xp5').style.opacity='0'
-        document.getElementById('xp3').style.right='75%'
+        document.getElementById('xp3').style.right='55%'
         document.getElementById('xp3').style.opacity='0'
         document.getElementById('xp4').style.right='50%'
         document.getElementById('xp4').style.opacity='1'
